82d850b7f3bdfe01f038d2d8e78b41e6c3d04c64
Alireza Valikhani <alirezavalikhabi1998@gmail.com>
2022-08-14 18:56:29 +0430

    Added correct import for Logger class

--- src/test/java/com/example/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,22 +1,21 @@
 package com.example.springjenkins;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.logging.Logger;
-
-import static org.junit.Assert.*;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
-    public static Logger logger = (Logger) LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
     @Test
     void contextLoads() {
         logger.info("Test case executing...");
-        assertEquals(true,true);
+        Assertions.assertTrue(true);
     }
 }
